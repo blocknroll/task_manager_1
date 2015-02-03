e20f66f835d4333fbd22b36c295c3bf414746ba5
Fred Block <info@fredblock.com>
2015-02-03 15:24:08 -0700

    Initial commit. Add 'dashboard' and 'task' routes. Add tasks 'index' to views.

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,13 @@
+class TaskManagerApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), "..")
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
+  end
+
+end
