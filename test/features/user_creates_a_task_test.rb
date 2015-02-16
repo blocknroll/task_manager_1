bbebfcf89f7e01c879d0783a9fc5b03921b41e96
Fred Block <info@fredblock.com>
2015-02-16 10:24:02 -0700

    Update 'user_creates_a_task_test.rb'.

--- test/features/user_creates_a_task_test.rb
+++ test/features/user_creates_a_task_test.rb
@@ -15,22 +15,4 @@ class UserCreatesTaskTest < FeatureTest
     end
   end
 
-  # def test_user_edits_a_task
-  #   TaskManager.create({:title => "updated title",
-  #                       :description => "updated description" })
-  #
-  #   visit '/tasks'
-  #   click_link_or_button('NEW TASK')
-  #   assert_equal '/tasks/new', current_path
-  #   fill_in 'task[title]', with: "Eat ice cream"
-  #   fill_in 'task[description]', with: 'yum'
-  #   click_link_or_button("Make a task!")
-  #   assert_equal '/tasks', current_path
-  #   # require 'pry'
-  #   # binding.pry
-  #   within('.task_panel') do
-  #     assert page.has_content?("Eat ice cream")
-  #   end
-  # end
-
 end
